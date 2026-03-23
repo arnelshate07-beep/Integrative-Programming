f01260956a047b23ad33a95f8c8863d8e1059497
arnelshate07-beep <arnelshate07@gmail.com>
2026-03-23 22:51:01 +0800

    Add files via upload

---
+++ WordGuessApplication/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WordGuessApplication
+{
+    partial class FrmGuessWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxGuess = new System.Windows.Forms.TextBox();
+            this.labelWord = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonGuess = new System.Windows.Forms.Button();
+            this.buttonHint = new System.Windows.Forms.Button();
+            this.listBoxWrong = new System.Windows.Forms.ListBox();
+            this.labelWrong = new System.Windows.Forms.Label();
+            this.labelHint = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxGuess
+            // 
+            this.textBoxGuess.Location = new System.Drawing.Point(152, 111);
+            this.textBoxGuess.Multiline = true;
+            this.textBoxGuess.Name = "textBoxGuess";
+            this.textBoxGuess.Size = new System.Drawing.Size(207, 20);
+            this.textBoxGuess.TabIndex = 0;
+            // 
+            // labelWord
+            // 
+            this.labelWord.AutoSize = true;
+            this.labelWord.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.labelWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWord.Location = new System.Drawing.Point(118, 43);
+            this.labelWord.Name = "labelWord";
+            this.labelWord.Size = new System.Drawing.Size(165, 42);
+            this.labelWord.TabIndex = 2;
+            this.labelWord.Text = "???????";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Guess:";
+            // 
+            // buttonGuess
+            // 
+            this.buttonGuess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGuess.Location = new System.Drawing.Point(45, 150);
+            this.buttonGuess.Name = "buttonGuess";
+            this.buttonGuess.Size = new System.Drawing.Size(152, 37);
+            this.buttonGuess.TabIndex = 4;
+            this.buttonGuess.Text = "Guess?";
+            this.buttonGuess.UseVisualStyleBackColor = false;
+            this.buttonGuess.Click += new System.EventHandler(this.buttonGuess_Click);
+            // 
+            // buttonHint
+            // 
+            this.buttonHint.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonHint.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonHint.Location = new System.Drawing.Point(203, 150);
+            this.buttonHint.Name = "buttonHint";
+            this.buttonHint.Size = new System.Drawing.Size(152, 37);
+            this.buttonHint.TabIndex = 5;
+            this.buttonHint.Text = "Hint?";
+            this.buttonHint.UseVisualStyleBackColor = false;
+            this.buttonHint.Click += new System.EventHandler(this.buttonHint_Click);
+            // 
+            // listBoxWrong
+            // 
+            this.listBoxWrong.FormattingEnabled = true;
+            this.listBoxWrong.Location = new System.Drawing.Point(41, 202);
+            this.listBoxWrong.Name = "listBoxWrong";
+            this.listBoxWrong.Size = new System.Drawing.Size(318, 95);
+            this.listBoxWrong.TabIndex = 6;
+            // 
+            // labelWrong
+            // 
+            this.labelWrong.AutoSize = true;
+            this.labelWrong.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.labelWrong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWrong.Location = new System.Drawing.Point(43, 311);
+            this.labelWrong.Name = "labelWrong";
+            this.labelWrong.Size = new System.Drawing.Size(73, 20);
+            this.labelWrong.TabIndex = 7;
+            this.labelWrong.Text = "Wrong: 0";
+            // 
+            // labelHint
+            // 
+            this.labelHint.AutoSize = true;
+            this.labelHint.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.labelHint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHint.Location = new System.Drawing.Point(301, 311);
+            this.labelHint.Name = "labelHint";
+            this.labelHint.Size = new System.Drawing.Size(55, 20);
+            this.labelHint.TabIndex = 8;
+            this.labelHint.Text = "Hint: 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 421);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Give up? or Start new? :";
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.buttonNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewGame.Location = new System.Drawing.Point(193, 421);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(152, 20);
+            this.buttonNewGame.TabIndex = 10;
+            this.buttonNewGame.Text = "New Game?";
+            this.buttonNewGame.UseVisualStyleBackColor = false;
+            this.buttonNewGame.Click += new System.EventHandler(this.buttonNewGame_Click);
+            // 
+            // FrmGuessWord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(403, 450);
+            this.Controls.Add(this.buttonNewGame);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelHint);
+            this.Controls.Add(this.labelWrong);
+            this.Controls.Add(this.listBoxWrong);
+            this.Controls.Add(this.buttonHint);
+            this.Controls.Add(this.buttonGuess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelWord);
+            this.Controls.Add(this.textBoxGuess);
+            this.Name = "FrmGuessWord";
+            this.Text = "FrmGuessWord";
+            this.Load += new System.EventHandler(this.FrmGuessWord_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxGuess;
+        private System.Windows.Forms.Label labelWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGuess;
+        private System.Windows.Forms.Button buttonHint;
+        private System.Windows.Forms.ListBox listBoxWrong;
+        private System.Windows.Forms.Label labelWrong;
+        private System.Windows.Forms.Label labelHint;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonNewGame;
+    }
+}
+
---
+++ WordGuessApplication/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordGuessApplication
+{
+    public partial class FrmGuessWord : Form
+    {
+        private string[] words = { "computer", "keyboard", "mouse", "monitor", "speaker" };
+
+        private string answer;
+        private StringBuilder hiddenWord;
+
+        private int wrongGuess = 0;
+        private int hintsLeft = 3;
+        private int revealIndex = 1;
+
+        private Random rnd = new Random();
+
+        public FrmGuessWord()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            answer = words[rnd.Next(words.Length)];
+
+            hiddenWord = new StringBuilder();
+
+            hiddenWord.Append(answer[0]);
+
+            for (int i = 1; i < answer.Length - 1; i++)
+            {
+                hiddenWord.Append("?");
+            }
+
+            hiddenWord.Append(answer[answer.Length - 1]);
+
+            labelWord.Text = hiddenWord.ToString();
+
+            listBoxWrong.Items.Clear();
+            textBoxGuess.Clear();
+
+            wrongGuess = 0;
+            hintsLeft = 3;
+            revealIndex = 1;
+
+            labelWrong.Text = "Wrong: 0";
+            labelHint.Text = "Hints: 3";
+
+            buttonGuess.Enabled = true;
+            buttonHint.Enabled = true;
+        }
+
+        private void FrmGuessWord_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGuess_Click(object sender, EventArgs e)
+        {
+            string input = textBoxGuess.Text.ToLower().Trim();
+
+            if (input == "")
+            {
+                MessageBox.Show("Enter a word.");
+                return;
+            }
+
+            if (input == answer)
+            {
+                labelWord.Text = answer;
+
+                MessageBox.Show("Correct! Play again?");
+
+                buttonGuess.Enabled = false;
+                buttonHint.Enabled = false;
+            }
+            else
+            {
+                wrongGuess++;
+
+                listBoxWrong.Items.Add(input);
+
+                labelWrong.Text = "Wrong: " + wrongGuess;
+
+                MessageBox.Show("Wrong guess!");
+            }
+
+            textBoxGuess.Clear();
+            textBoxGuess.Focus();
+        }
+
+        private void buttonHint_Click(object sender, EventArgs e)
+        {
+            if (hintsLeft > 0)
+            {
+                if (revealIndex < answer.Length - 1)
+                {
+                    hiddenWord[revealIndex] = answer[revealIndex];
+                    revealIndex++;
+                }
+
+                labelWord.Text = hiddenWord.ToString();
+
+                hintsLeft--;
+                labelHint.Text = "Hints: " + hintsLeft;
+
+                if (hintsLeft == 0)
+                {
+                    buttonHint.Enabled = false;
+                }
+            }
+        }
+
+        private void buttonNewGame_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+    }
+}

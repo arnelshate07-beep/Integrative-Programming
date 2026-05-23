8ee2838eb4c1f1d9917873e89dbbc42f63f7d42a
arnelshate07-beep <arnelshate07@gmail.com>
2026-05-23 09:59:41 +0800

    Add files via upload

---
+++ CashierApplication/CashierApplication/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashierApplication
+{
+    public class Item
+    {
+        protected string item_name;
+        protected double item_price;
+        protected int item_quantity;
+        private double total_price;
+
+        public Item(string item_name, double price, int quantity)
+        {
+            this.item_name = item_name;
+            this.item_price = price;
+            this.item_quantity = quantity;
+        }
+
+        public virtual double GetTotalPrice()
+        {
+            total_price = item_price * item_quantity;
+            return total_price;
+        }
+
+        public virtual void setPayment(double amount)
+        {
+
+        }
+    }
+}
